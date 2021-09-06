8f248c40101daadd9824df7f5ae9fc6639bd93de
nousvang <80865244+nousvang@users.noreply.github.com>
2021-09-06 10:11:13 -0500

    added github url

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherApp />
+      <p>
+        Coded by Nou Vang and is{" "}
+        <a
+          href="https://github.com/nousvang/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          opened-sourced.
+        </a>
+      </p>
     </div>
   );
 }
